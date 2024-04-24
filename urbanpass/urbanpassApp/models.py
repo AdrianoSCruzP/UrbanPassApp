f6f58e37280862442a1f1a74005535794173a0ab
Adriano Sebastian Cruz Palomino <u202210697@upc.edu.pe>
2024-04-24 14:32:17 -0500

    fix: bad request

--- urbanpass/urbanpassApp/models.py
+++ urbanpass/urbanpassApp/models.py
@@ -1,10 +1,6 @@
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
+# Create your models here.
 
 class Cliente(models.Model):
     id_cliente = models.AutoField(db_column='ID_Cliente', primary_key=True)  # Field name made lowercase.
